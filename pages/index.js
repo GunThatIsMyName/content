3f4e326a9d669643d8465289fca1618f37ca6751
user <user@userui-MacBookPro.local>
2022-07-31 16:06:32 +0900

    Generating Paths

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
-export default function Recipes() {
+// setup connection with contentful
+import { createClient } from 'contentful'
+
+// Components
+import RecipeCard from '../components/RecipeCard'
+
+export const getStaticProps=async()=>{
+  const client = createClient({
+    space:process.env.CONTENTFUL_SPACE_ID,
+    accessToken:process.env.CONTENTFUL_API_KEY,
+  })
+
+  // Retrieve all entries of a space
+  const response = await client.getEntries({
+    content_type:'recipe'
+  })
+  return {
+    props:{
+      recipes:response.items
+    }
+  }
+}
+
+const index = ({recipes}) => {
   return (
-    <div className="recipe-list">
-      Recipe List
+    <div className='recipe-list' >
+      {recipes.map(recipe=>{
+        return <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      })}
+
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
     </div>
   )
 }
+
+export default index
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,51 @@
-export default function RecipeDetails() {
+import { createClient } from 'contentful';
+import React from 'react'
+
+const client = createClient({
+  space:process.env.CONTENTFUL_SPACE_ID,
+  accessToken:process.env.CONTENTFUL_API_KEY,
+})
+
+const RecipeDetail = ({recipe}) => {
+  console.log(recipe,"recipe")
   return (
-    <div>
-      Recipe Details
-    </div>
+    <div>RecipeDetail</div>
   )
+}
+
+export default RecipeDetail;
+
+// props
+export const getStaticProps = async({params}) =>{
+  console.log(params,"@context");
+
+
+  const res = await client.getEntries({
+    content_type:'recipe',
+    'fields.slug':params.slug
+  });
+  return {
+    props:{
+      recipe:res.items[0]
+    }
+  }
+
+}
+// to build Static Pages each path
+export const getStaticPaths = async() =>{
+  const response = await client.getEntries({
+    content_type:'recipe'
+  })
+
+  const paths = response.items.map(item=>{
+    return {
+      params:{slug:item.fields.slug}
+    }
+  })
+
+  // paths should return array
+  return {
+    paths,
+    fallback:false
+  }
 }
